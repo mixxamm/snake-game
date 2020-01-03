6f0fad5b0fb014cd37b45bd9d90c786f5cf1d37a
mixxamm <maximjanssensj5@gmail.com>
2020-01-03 12:09:36 +0100

    Maak swipe voor gsm

--- s0203020-snake/src/js/site.js
+++ s0203020-snake/src/js/site.js
@@ -28,6 +28,46 @@ $(document).keydown(e => {
 
 });
 
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+const gestureZone = document.getElementById('swipe-detector');
+
+gestureZone.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+gestureZone.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false); 
+
+function handleGesture() {
+    if (touchendX < touchstartX) {
+        direction = "left";
+    }
+    
+    if (touchendX > touchstartX) {
+        direction = "right";
+    }
+    
+    if (touchendY < touchstartY) {
+        direction = "up";
+    }
+    
+    if (touchendY > touchstartY) {
+        direction = "down";
+    }
+    
+    if (touchendY === touchstartY) {
+       console.log('Tap');
+    }
+}
+
 
 
 moveSnake = () => {
